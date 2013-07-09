7c1c27488c42d35cbaa76299439f359119a9d349
Tim Sporcic <timsporcic@gmail.com>
2013-07-08 21:22:33 -0500

    Added index.html

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
-var express = require('express')
-  , http = require('http');
+var express = require('express'),
+    fs = require('fs'),
+    http = require('http');
 
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(express.logger('dev'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+var contents = fs.readFileSync('index.html');
 
+app.get('/', function(request, response) {
+  response.send( contents.toString('utf-8') );
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
